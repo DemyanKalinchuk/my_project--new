76433482d62524fd5fa3422d941bcd1a5c7581f6
Demian Kalinchuk <demonation20@gmail.com>
2021-11-07 18:48:28 +0200

    new_class

--- src/com/company/car.java
+++ src/com/company/car.java
@@ -2,15 +2,15 @@ package com.company;
 
 public class car {
     public static String hello = "info car";
-   protected String model;
-   protected String marka;
+    protected String model;
+    protected String marka;
     int yers;
     void KPP (String str){
-System.out.println(str);
+        System.out.println(str);
     }
-     car (String model, String marka){
-this.model = model;
-this.marka = marka;
+    car (String model, String marka){
+        this.model = model;
+        this.marka = marka;
 
     }
     car (){}
