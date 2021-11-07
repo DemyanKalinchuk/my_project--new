437eaa639b00d1ee4310d3573fdf6e859a30bf96
Demian Kalinchuk <demonation20@gmail.com>
2021-11-07 18:14:16 +0200

    OOP

---
+++ src/com/company/Main.java
@@ -0,0 +1,32 @@
+package com.company;
+
+import java.awt.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+car sport = new car("R8","audi");
+car.hello = "info car: -c";
+System.out.println(car.hello);
+sport.yers = 2015;
+System.out.println("Sport car is: ");
+System.out.println(sport.marka = "audi");
+System.out.println(sport.model = "R8");
+sport.KPP("This type car have a Tiptronik KPP!");
+System.out.println(sport.yers = 2015);
+
+car havy = new car();
+
+havy.marka = "MAZ";
+havy.model = "Kontinent";
+havy.yers = 2010;
+        System.out.println("Havy car is: ");
+        System.out.println(sport.marka = "MAZ");
+        System.out.println(sport.model = "Kontinent");
+        sport.KPP("This type car have a mechanics KPP!");
+        System.out.println(sport.yers = 2010);
+        type_car sportcar = new type_car(195,"A4","Audi","cabriolet",2.5f);
+        System.out.println(sportcar.type);
+}
+    }
