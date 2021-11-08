d968044d34b47051f3f697e7cef824aed1bab695
Demian Kalinchuk <demonation20@gmail.com>
2021-11-08 20:39:12 +0200

    change class

--- src/havy_car.java
+++ src/havy_car.java
@@ -1,3 +1,12 @@
-public class havy_car {
+import com.company.type_car;
 
+public class havy_car extends type_car {
+
+    String type;
+    int height ;
+    float weight;
+
+    public havy_car(int height, String model, String marka, String type, float weight) {
+        super(height, model, marka, type, weight);
+    }
 }
